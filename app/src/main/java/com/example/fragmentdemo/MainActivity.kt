dd16afe25c1cc7b00bc7adc918751125476cf5c9
adgod000 <seahmx-wm19@student.tarc.edu.my>
2021-02-11 01:55:57 +0800

    init

---
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.fragmentdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val welcomefg = WelcomeFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.myFrame,welcomefg)
+            commit()
+        }
+
+    }
+}
